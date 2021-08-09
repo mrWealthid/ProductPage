aad82ec5ea55203adc4fe94c7d8a7a3f551015cb
Wealth Iduwe <wealthiduwe@gmail.com>
2021-08-09 03:16:33 +0100

    productList

--- src/Views/Product.js
+++ src/Views/Product.js
@@ -1,56 +1,56 @@
-// import React, { useState, useEffect } from "react";
-// // import { HttpRequest } from "../Request/HttpRequest";
-// import axios from "axios";
-// import Loader from "../components/Loader";
-// import ProductCard from "../components/ProductCard";
+import React, { useState, useEffect } from "react";
+// import { HttpRequest } from "../Request/HttpRequest";
+import axios from "axios";
+import Loader from "../components/Loader";
+import ProductCard from "../components/ProductCard";
 
-// const Product = () => {
-//   const [product, setProduct] = useState({
-//     loading: false,
-//     data: null,
-//     error: false,
-//   });
+const Product = () => {
+  const [product, setProduct] = useState({
+    loading: false,
+    data: null,
+    error: false,
+  });
 
-//   const url = "https://5e9623dc5b19f10016b5e31f.mockapi.io/api/v1/products/";
+  const url = "https://5e9623dc5b19f10016b5e31f.mockapi.io/api/v1/products/";
 
-//   let content = null;
-//   useEffect(() => {
-//     setProduct({
-//       loading: true,
-//       data: null,
-//       error: false,
-//     });
-//     axios
-//       .get(url)
-//       .then(({ data }) => {
-//         console.log(data);
-//         setProduct({ loading: false, data: data, error: false });
-//       })
-//       .catch(() =>
-//         setProduct({
-//           loading: false,
-//           data: null,
-//           error: true,
-//         })
-//       );
-//   }, [url]);
+  let content = null;
+  useEffect(() => {
+    setProduct({
+      loading: true,
+      data: null,
+      error: false,
+    });
+    axios
+      .get(url)
+      .then(({ data }) => {
+        console.log(data);
+        setProduct({ loading: false, data: data, error: false });
+      })
+      .catch(() =>
+        setProduct({
+          loading: false,
+          data: null,
+          error: true,
+        })
+      );
+  }, [url]);
 
-//   console.log(product);
+  console.log(product);
 
-//   //checking for truthy value and reassigning the content for proper error handling
-//   product.loading && (content = <Loader />);
+  //checking for truthy value and reassigning the content for proper error handling
+  product.loading && (content = <Loader />);
 
-//   product.error && (content = <div>There was an error loading data</div>);
+  product.error && (content = <div>There was an error loading data</div>);
 
-//   return (
-//     <div>
-//       {product.data
-//         ? product.data.map((item) => (
-//             <ProductCard key={item.id} item={item} product={product} />
-//           ))
-//         : content}
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      {product.data
+        ? product.data.map((item) => (
+            <ProductCard key={item.id} item={item} product={product} />
+          ))
+        : content}
+    </div>
+  );
+};
 
-// export default Product;
+export default Product;
